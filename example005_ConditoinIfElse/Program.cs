eeb08431826f8c8aa2ce21eaa6d0f22a0ec4a3db
VassiliyZh <111jvv@mail.ru>
2022-12-15 14:18:47 +0600

    Программа с использованием условий если, иначе

---
+++ example005_ConditoinIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите свое имя");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Привет, МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, "); 
+    Console.WriteLine(username);
+}
